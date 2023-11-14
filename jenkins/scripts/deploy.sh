085b14e9abe1d1f7295723f443e69fad642fb19d
wayell <97503567+wayell@users.noreply.github.com>
2023-11-15 03:47:01 +0800

    init

---
+++ jenkins/scripts/deploy.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+
+set -x
+docker run -d -p 80:80 --name my-apache-php-app php:7.2-apache
+docker cp ./src/. my-apache-php-app:/var/www/html
+sleep 1
+set +x
+
+echo 'Now...'
+echo 'Visit http://localhost to see your PHP application in action.'
